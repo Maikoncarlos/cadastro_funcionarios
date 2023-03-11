d5fd37f95b39c887e720e88c613d44aaea28d682
Maikoncarlos <maikonsaturno@gmail.com>
2023-03-11 18:03:45 -0300

    first commit

---
+++ src/test/java/com/github/maikoncarlos/cadastro_funcionarios/CadastroFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maikoncarlos.cadastro_funcionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
