780ac41b77c641f86516d0ba91c17862028eceb3
Maikoncarlos <maikonsaturno@gmail.com>
2023-03-11 19:22:42 -0300

    add anotação Entity na entidade para podermos persistir na base de dados

--- src/main/java/com/github/maikoncarlos/cadastro_funcionarios/repository/entity/FuncionarioRequestEntity.java
+++ src/main/java/com/github/maikoncarlos/cadastro_funcionarios/repository/entity/FuncionarioRequestEntity.java
@@ -1,15 +1,21 @@
 package com.github.maikoncarlos.cadastro_funcionarios.repository.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
-@Builder
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class FuncionarioRequestEntity {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = "id", nullable = false)
+    private Long id;
 
     private String name;
 }
